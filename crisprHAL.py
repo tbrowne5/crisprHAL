ec67778031a99d850a776692b87e83f764945b7f
Tyler Browne <44354282+tbrowne5@users.noreply.github.com>
2025-06-11 14:04:38 -0400

    Update crisprHAL.py

--- crisprHAL.py
+++ crisprHAL.py
@@ -31,7 +31,7 @@ def parse_args(args):
                 if args[i + 1].upper() in ["TEVSPCAS9","TEVCAS9", "TEV", "SPCAS9"]: modelName = "TEVSPCAS9"
                 if args[i + 1].upper() in ["WT-SPCAS9", "WILD-TYPE-SPCAS9","WILD-TYPE","WILDTYPE", "WT"]: modelName = "WT-SPCAS9"
                 if args[i + 1].upper() in ["ESPCAS9", "ESP"]: modelName = "ESPCAS9"
-                if arge[i + 1].upper() in ["TEVSACAS9","SACAS9","SACAS","SA"]: modelName = "TEVSACAS9"
+                if args[i + 1].upper() in ["TEVSACAS9","SACAS9","SACAS","SA"]: modelName = "TEVSACAS9"
             else:
                 print(f"Error: Model '{args[i + 1]}' is not recognized. Available models: TevSpCas9, eSpCas9, and WT-SpCas9.\nFor up-to-date and other models including SaCas9 please use: github.com/tbrowne5/crisprHAL")
                 sys.exit(1)
