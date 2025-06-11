f219ccebb99e193550c0fb1cd594b16128c3a04c
Tyler Browne <44354282+tbrowne5@users.noreply.github.com>
2025-06-11 15:12:29 -0400

    Update crisprHAL.py

--- crisprHAL.py
+++ crisprHAL.py
@@ -1,5 +1,10 @@
 import sys
 import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6" # Specifies which GPU to use
+os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async" # may need: export TF_GPU_ALLOCATOR=cuda_malloc_async
+
 from models import models, modelVersionDefaultEpochs
 from processing import processing
 
